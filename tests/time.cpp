ac6b25ba6bdee2ab78b76a1bb17174cdcefc3802
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2021-03-09 08:29:52 +0100

    Added test for the time nearest neighbor

--- tests/time.cpp
+++ tests/time.cpp
@@ -1,3 +1,5 @@
+#include <chrono>
+
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
@@ -6,8 +8,36 @@
 namespace gos {
 namespace time {
 
+typedef ::std::chrono::high_resolution_clock clock;
+typedef clock::duration duration;
+typedef clock::time_point time;
+
+const duration moment = duration();
 TEST(GosTimeTest, NearestNeighbor) {
-  double first, second, value;
+  time first, second, value;
+  duration result;
+
+  value = clock::now();
+
+  first = value + ::std::chrono::minutes(10);
+  second = value + ::std::chrono::minutes(20);
+  result = ::gos::time::nearest::neighbor<time, duration>(first, second, value);
+  EXPECT_GT(moment, result);
+
+  first = value - ::std::chrono::minutes(10);
+  second = value + ::std::chrono::minutes(25);
+  result = ::gos::time::nearest::neighbor<time, duration>(first, second, value);
+  EXPECT_GT(moment, result);
+
+  first = value - ::std::chrono::minutes(15);
+  second = value + ::std::chrono::minutes(10);
+  result = ::gos::time::nearest::neighbor<time, duration>(first, second, value);
+  EXPECT_LT(moment, result);
+
+  first = value - ::std::chrono::minutes(25);
+  second = value - ::std::chrono::minutes(10);
+  result = ::gos::time::nearest::neighbor<time, duration>(first, second, value);
+  EXPECT_LT(moment, result);
 }
 
 } // namespace time
